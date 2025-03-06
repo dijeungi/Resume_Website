15bb0688ffc86776a280ed0b4d810331c0f50141
dijeungi <dijeungi@naver.com>
2025-03-06 19:23:11 +0900

    프로젝트 초기화

---
+++ src/app/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+
+function App() {
+    return (
+        // Container
+        <>
+            <div className="App_Header">
+                <div className="Header_Left">
+                    최준호
+                    <div className="Left_Sub">프론트엔드 개발자</div>
+                </div>
+                <div className="Header_Right">
+                    <div className="Right_Phone">연락처: 010-5357-7933</div>
+                    <div className="Right_Email">이메일: junho_0211@naver.com</div>
+                </div>
+            </div>
+
+            {/* Container */}
+            <div className="App_Content">
+                <div className="Content_Title">Introduce</div>
+                <hr />
+                <div className="Content_Photo">
+                    <img src="" />
+                </div>
+                <div className="Content_Text"></div>
+            </div>
+        </>
+    );
+}
+
+export default App;
